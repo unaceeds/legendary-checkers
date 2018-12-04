cc06c2f8da9b34e609fab1b3c55bef88cd725763
unaceeds <michaelgineciv@gmail.com>
2018-12-04 14:15:42 +0100

    Tescik update

---
+++ checkers/src/test/java/checkers/Tescik.java
@@ -0,0 +1,18 @@
+package checkers;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class Tescik {
+
+	public Tescik() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Test
+	public void methTest2() {
+		assertEquals(true, true);
+	}
+
+}
--- checkers/src/test/java/checkers/Test.java
+++
@@ -1,9 +0,0 @@
-package checkers;
-
-public class Test {
-
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-
-}

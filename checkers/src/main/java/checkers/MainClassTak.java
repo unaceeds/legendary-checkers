43fb5e374431c1c37194e5a170d09c5ca23abfc6
unaceeds <michaelgineciv@gmail.com>
2018-12-04 14:48:13 +0100

    Created first class in the main dirctory

---
+++ checkers/src/main/java/checkers/MainClassTak.java
@@ -0,0 +1,14 @@
+package checkers;
+
+public class MainClassTak {
+
+	public MainClassTak() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
